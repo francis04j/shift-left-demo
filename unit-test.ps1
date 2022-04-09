5caa023aaf26ba89fbfb8a7ca9fa128c381b6fae
gitName <a@b.c>
2022-04-09 01:29:44 +0100

    unit test build

--- unit-test.ps1
+++ unit-test.ps1
@@ -1,7 +1,7 @@
 $IMAGE_TAG= Read-Host -Prompt 'Input image name and tag (e.g. hello:v1)'
 
 Write-Output "Buiding unit-test stage with '$IMAGE_TAG'"
-docker build -f Dockerfile.unit-tests --target unit-test -t $IMAGE_TAG
+docker build -f Dockerfile.unit-tests --target unit-test -t $IMAGE_TAG .
 
 Write-Output "Running unit tests"
 docker run --rm -v "${pwd}\TestResults:/code/test/DanApi.UnitTests/TestResults" $IMAGE_TAG
